2c0c66c567a47b36642a13a795028012f4e5d897
Srickshaw <derek.gulledge@gmail.com>
2016-05-26 06:06:46 -0500

    Replaced all tabs with 2/4/6/8 spaces

--- dev/js/itemGen.js
+++ dev/js/itemGen.js
@@ -1,277 +1,278 @@
 const gId = (elemId) => {
-	return document.getElementById(elemId);
+  return document.getElementById(elemId);
 }
 
 const checkProps = (itemObj, gearPiece, qual) => {
-	if(!itemObj.qualityLevels[qual].applicableGearTypes.hasOwnProperty(gearPiece.type) ||
-	itemObj.qualityLevels[qual].tier > gearPiece.tier ||
-	!itemObj.qualityLevels[qual].applicableMaterial.hasOwnProperty(gearPiece.materialType)) {
-		return false;
-	}
-	else {
-		return true;
-	}
+  if(!itemObj.qualityLevels[qual].applicableGearTypes.hasOwnProperty(gearPiece.type) ||
+  itemObj.qualityLevels[qual].tier > gearPiece.tier ||
+  !itemObj.qualityLevels[qual].applicableMaterial.hasOwnProperty(gearPiece.materialType)) {
+    return false;
+  }
+  else {
+    return true;
+  }
 }
 
 const checkPreSuf = (itemObj, gearPiece, pre, suf) => {
-	if (itemObj.prefixes[pre].tier > gearPiece.tier && itemObj.suffixes[suf].tier > gearPiece.tier) {
-		return false;
-	}
-	else {
-		const itemPreSuf = [];
-		itemPreSuf.push(itemObj.prefixes[pre]);
-		itemPreSuf.push(itemObj.suffixes[suf]);
-		return itemPreSuf;
+  if (itemObj.prefixes[pre].tier > gearPiece.tier && itemObj.suffixes[suf].tier > gearPiece.tier) {
+    return false;
+  }
+  else {
+    const itemPreSuf = [];
+    itemPreSuf.push(itemObj.prefixes[pre]);
+    itemPreSuf.push(itemObj.suffixes[suf]);
+    return itemPreSuf;
 	}
 }
 
 const checkElemMod = (elemMod, gearPiece) => {
-	if (elemMod.tier > gearPiece.tier) {
-		return false;
-	}
-	else {
-		return elemMod;
-	}
+  if (elemMod.tier > gearPiece.tier) {
+    return false;
+  }
+  else {
+    return elemMod;
+  }
 }
 
 const getGearType = (itemObj, gearType) => {
-	return itemObj.itemTypes[gearType];
+  return itemObj.itemTypes[gearType];
 }
 
 const generateBaseItem = (itemObj, gearType) => {
-	const gGearType = getGearType(itemObj, gearType);
-	const itemTypePos = randomNum(0, (gGearType.length - 1));
-	return itemObj.itemTypes[gearType][itemTypePos];
+  const gGearType = getGearType(itemObj, gearType);
+  const itemTypePos = randomNum(0, (gGearType.length - 1));
+  return itemObj.itemTypes[gearType][itemTypePos];
 }
 
 const generateMaterial = (itemObj, gearPiece) => {
-	const itemMatPos = randomNum(0, (itemObj.itemMaterials[gearPiece.materialType].length - 1));
-	return itemObj.itemMaterials[gearPiece.materialType][itemMatPos];
+  const itemMatPos = randomNum(0, (itemObj.itemMaterials[gearPiece.materialType].length - 1));
+  return itemObj.itemMaterials[gearPiece.materialType][itemMatPos];
 }
 
 const generateItem = (itemObj, gearType) => {
-	const generatedItem = {};
-	generatedItem["item"] = generateBaseItem(itemObj, gearType);
-	generatedItem["material"] = generateMaterial(itemObj, generatedItem["item"]);
-	let itemQualityPos =  generateQuality(itemObj);
-	let iProps = checkProps(itemObj, generatedItem["item"], itemQualityPos);
-	while(iProps === false) {
-		itemQualityPos =  generateQuality(itemObj);
-		iProps = checkProps(itemObj, generatedItem["item"], itemQualityPos);
-	}
-	generatedItem["quality"] = itemObj.qualityLevels[itemQualityPos];
-	return generatedItem;
+  const generatedItem = {};
+  generatedItem["item"] = generateBaseItem(itemObj, gearType);
+  generatedItem["material"] = generateMaterial(itemObj, generatedItem["item"]);
+  let itemQualityPos =  generateQuality(itemObj);
+  let iProps = checkProps(itemObj, generatedItem["item"], itemQualityPos);
+  while(iProps === false) {
+    itemQualityPos =  generateQuality(itemObj);
+    iProps = checkProps(itemObj, generatedItem["item"], itemQualityPos);
+  }
+  generatedItem["quality"] = itemObj.qualityLevels[itemQualityPos];
+  return generatedItem;
 }
 
 const generateQuality = (itemObj) => {
-	const iQuality = randomNum(0, (itemObj.qualityLevels.length - 1));
-	return iQuality;
+  const iQuality = randomNum(0, (itemObj.qualityLevels.length - 1));
+  return iQuality;
 }
 
 const generatePrefix = (itemObj) => {
-	const itemPrefix = randomNum(0, (itemObj.prefixes.length - 1));
-	return itemPrefix;
+  const itemPrefix = randomNum(0, (itemObj.prefixes.length - 1));
+  return itemPrefix;
 }
 
 const generateSuffix = (itemObj) => {
-	const itemSuffix = randomNum(0, (itemObj.prefixes.length - 1));
-	return itemSuffix;
+  const itemSuffix = randomNum(0, (itemObj.prefixes.length - 1));
+  return itemSuffix;
 }
 
 const addPreSuf = (itemObj, gearPiece, generatedItem) => {
-	let check = checkPreSuf(itemObj, gearPiece, generatePrefix(itemObj), generateSuffix(itemObj));
-	while (check == false) {
-		check = checkPreSuf(itemObj, gearPiece, generatePrefix(itemObj), generateSuffix(itemObj))
+  let check = checkPreSuf(itemObj, gearPiece, generatePrefix(itemObj), generateSuffix(itemObj));
+  while (check == false) {
+    check = checkPreSuf(itemObj, gearPiece, generatePrefix(itemObj), generateSuffix(itemObj))
 	}
-	generatedItem['prefix'] = check[0];
-	generatedItem['suffix'] = check[1];
+  generatedItem['prefix'] = check[0];
+  generatedItem['suffix'] = check[1];
 }
 
 const calculateModifiers = (generatedItem) => {
-	generatedItem['modifiers'] = {};
-	if (generatedItem.prefix.name != '') {
-		Object.keys(generatedItem.prefix.statMod).forEach((index) => {
-			generatedItem['modifiers'][generatedItem.prefix.statMod[index]] = randomNum(generatedItem.prefix.lowerModAmt[index],
-				generatedItem.prefix.upperModAmt[index]);
-			});
-		}
-	if (generatedItem.suffix.name != '') {
-		Object.keys(generatedItem.suffix.statMod).forEach((index) => {
-			const calculatedSuffixMod = randomNum(generatedItem.suffix.lowerModAmt[index], generatedItem.suffix.upperModAmt[index])
-			if (generatedItem['modifiers'].hasOwnProperty(generatedItem.suffix.statMod[index])) {
-				generatedItem['modifiers'][generatedItem.suffix.statMod[index]] += calculatedSuffixMod;
-			}
-			else {
-				generatedItem['modifiers'][generatedItem.suffix.statMod[index]] = calculatedSuffixMod;
-			}
-		});
-	}
+  generatedItem['modifiers'] = {};
+  if (generatedItem.prefix.name != '') {
+    Object.keys(generatedItem.prefix.statMod).forEach((index) => {
+       generatedItem['modifiers'][generatedItem.prefix.statMod[index]] = randomNum(generatedItem.prefix.lowerModAmt[index],
+       generatedItem.prefix.upperModAmt[index]);
+    });
+  }
+  if (generatedItem.suffix.name != '') {
+    Object.keys(generatedItem.suffix.statMod).forEach((index) => {
+      const calculatedSuffixMod = randomNum(generatedItem.suffix.lowerModAmt[index], generatedItem.suffix.upperModAmt[index])
+      if (generatedItem['modifiers'].hasOwnProperty(generatedItem.suffix.statMod[index])) {
+        generatedItem['modifiers'][generatedItem.suffix.statMod[index]] += calculatedSuffixMod;
+      }
+      else {
+        generatedItem['modifiers'][generatedItem.suffix.statMod[index]] = calculatedSuffixMod;
+      }
+    });
+  }
 }
 
 const generateElemMod = (itemObj) => {
-	const elemMod = randomNum(0, (itemObj.elemMods.length - 1));
-	return elemMod;
+  const elemMod = randomNum(0, (itemObj.elemMods.length - 1));
+  return elemMod;
 }
 
 const addElemMod = (itemObj, gearPiece, generatedItem) => {
-	let checkElem = checkElemMod(itemObj.elemMods[generateElemMod(itemObj)], gearPiece);
-	while(checkElem === false) {
-		checkElem = checkElemMod(itemObj.elemMods[generateElemMod(itemObj)], gearPiece);
-	}
-	generatedItem['elemMod'] = checkElem;
+  let checkElem = checkElemMod(itemObj.elemMods[generateElemMod(itemObj)], gearPiece);
+  while(checkElem === false) {
+    checkElem = checkElemMod(itemObj.elemMods[generateElemMod(itemObj)], gearPiece);
+  }
+  generatedItem['elemMod'] = checkElem;
 }
 
 const generateItemName = (generatedItem) => {
-	const itemName = `${generatedItem["elemMod"].name} ${generatedItem["quality"].name} ${generatedItem["prefix"].name} ${generatedItem["material"].name} ${generatedItem["item"].name} ${generatedItem["suffix"].name}`;
-	generatedItem["name"] = itemName;
+  const itemName = `${generatedItem["elemMod"].name} ${generatedItem["quality"].name} ${generatedItem["prefix"].name} ${generatedItem["material"].name} ${generatedItem["item"].name} ${generatedItem["suffix"].name}`;
+  generatedItem["name"] = itemName;
 }
 
 const calculateDamArm = (generatedItem) => {
-	if(generatedItem.item.type == 'weapon') {
-		let baseMinDam = Math.floor((generatedItem.item.minDam + generatedItem.material.minDamMod) * generatedItem.quality.damMod);
-		let baseMaxDam = Math.floor((generatedItem.item.maxDam + generatedItem.material.maxDamMod) * generatedItem.quality.damMod);
-		if (generatedItem.modifiers.hasOwnProperty('damage')) {
-			baseMinDam = Math.floor(baseMinDam + ( baseMinDam * (generatedItem.modifiers["Damage"] / 100)));
-			baseMaxDam = Math.floor(baseMinDam + ( baseMinDam * (generatedItem.modifiers["Damage"] / 100)));
-		}
-		if (generatedItem.elemMod.name != '') {
-			baseMinDam = Math.floor(baseMinDam + (baseMinDam * generatedItem.elemMod.damMod));
-			baseMaxDam = Math.floor(baseMaxDam + (baseMaxDam * generatedItem.elemMod.damMod));
-		}
-		generatedItem['minDam'] = baseMinDam;
-		generatedItem['maxDam'] = baseMaxDam;
-	}
-	else if(generatedItem.item.type == 'armor') {
-		const baseArmor = randomNum(generatedItem.item.minArmor, generatedItem.item.maxArmor);
-		if (generatedItem.elemMod.name != '') {
-			generatedItem['modifiers'][`${generatedItem.elemMod.elemType} Resistance`] = generatedItem.elemMod.resistance;
-		}
-		generatedItem['armorValue'] = Math.floor((baseArmor + generatedItem.material.armorMod) * generatedItem.quality.armorMod);
-	}
+  if(generatedItem.item.type == 'weapon') {
+    let baseMinDam = Math.floor((generatedItem.item.minDam + generatedItem.material.minDamMod) * generatedItem.quality.damMod);
+    let baseMaxDam = Math.floor((generatedItem.item.maxDam + generatedItem.material.maxDamMod) * generatedItem.quality.damMod);
+    if (generatedItem.modifiers.hasOwnProperty('damage')) {
+      baseMinDam = Math.floor(baseMinDam + ( baseMinDam * (generatedItem.modifiers["Damage"] / 100)));
+      baseMaxDam = Math.floor(baseMinDam + ( baseMinDam * (generatedItem.modifiers["Damage"] / 100)));
+    }
+    if (generatedItem.elemMod.name != '') {
+      baseMinDam = Math.floor(baseMinDam + (baseMinDam * generatedItem.elemMod.damMod));
+      baseMaxDam = Math.floor(baseMaxDam + (baseMaxDam * generatedItem.elemMod.damMod));
+    }
+    generatedItem['minDam'] = baseMinDam;
+    generatedItem['maxDam'] = baseMaxDam;
+  }
+  else if(generatedItem.item.type == 'armor') {
+    const baseArmor = randomNum(generatedItem.item.minArmor, generatedItem.item.maxArmor);
+    if (generatedItem.elemMod.name != '') {
+      generatedItem['modifiers'][`${generatedItem.elemMod.elemType} Resistance`] = generatedItem.elemMod.resistance;
+    }
+    generatedItem['armorValue'] = Math.floor((baseArmor + generatedItem.material.armorMod) * generatedItem.quality.armorMod);
+  }
 }
 
 const calculateQuality = (generatedItem) => {
-	generatedItem['qualityLevel'] = Math.floor((generatedItem.item.baseQuality + generatedItem.material.qualityMod +
-	generatedItem.elemMod.qualityMod + generatedItem.prefix.qualityMod + generatedItem.suffix.qualityMod) *
-	generatedItem.quality.qualityMod);
+  generatedItem['qualityLevel'] = Math.floor((generatedItem.item.baseQuality + generatedItem.material.qualityMod +
+  generatedItem.elemMod.qualityMod + generatedItem.prefix.qualityMod + generatedItem.suffix.qualityMod) *
+  generatedItem.quality.qualityMod);
 };
 
 const calculateCost = (generatedItem) => {
-	generatedItem['cost'] = Math.ceil((generatedItem.item.baseCost * generatedItem.material.costMod *
-		generatedItem.elemMod.costMod * generatedItem.prefix.costMod * generatedItem.suffix.costMod) *
-		generatedItem.quality.costMod);
+  generatedItem['cost'] = Math.ceil((generatedItem.item.baseCost * generatedItem.material.costMod *
+    generatedItem.elemMod.costMod * generatedItem.prefix.costMod * generatedItem.suffix.costMod) *
+    generatedItem.quality.costMod);
 }
 
 const generateWeapon = (weapon, type) => {
-	const newGenItem = generateItem(weapon, type);
-	addPreSuf(weapon, newGenItem.item, newGenItem);
-	calculateModifiers(newGenItem);
-	addElemMod(weapon, newGenItem.item, newGenItem);
-	generateItemName(newGenItem);
-	calculateDamArm(newGenItem);
-	calculateQuality(newGenItem);
-	calculateCost(newGenItem);
-	console.log(newGenItem);
-	return newGenItem;
+  const newGenItem = generateItem(weapon, type);
+  addPreSuf(weapon, newGenItem.item, newGenItem);
+  calculateModifiers(newGenItem);
+  addElemMod(weapon, newGenItem.item, newGenItem);
+  generateItemName(newGenItem);
+  calculateDamArm(newGenItem);
+  calculateQuality(newGenItem);
+  calculateCost(newGenItem);
+  console.log(newGenItem);
+  return newGenItem;
 }
 
 const randomNum = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const prettifyModifiers = (arr, obj) => {
-	return arr.map((index, value) => {
-		if(obj[index] < 1) {
-			return '+' + Math.floor(obj[index] * 100) + '% ' + index;
-		}
-		else if (index === 'Damage') {
-			return '+' + obj[index] + '% Increased Weapon ' + index;
-		}
-		else {
-			return '+' + obj[index] + ' ' + index;
-		}
-	});
+  return arr.map((index, value) => {
+    if(obj[index] < 1) {
+      return '+' + Math.floor(obj[index] * 100) + '% ' + index;
+    }
+    else if (index === 'Damage') {
+      return '+' + obj[index] + '% Increased Weapon ' + index;
+    }
+    else {
+      return '+' + obj[index] + ' ' + index;
+    }
+  });
 }
 
 const createComponent = (type, children, classArr = []) => {
-	let newElement = document.createElement(type);
-	classArr.forEach((cssClass) =>
-	newElement.setAttribute('class', cssClass)
-	)
-	children.forEach((index) => {
-		if (typeof index === 'string') {
-			let newText = document.createTextNode(index);
-			newElement.appendChild(newText);
-		}
-		else {
-			newElement.appendChild(index)
-		}
-	});
-	return newElement;
+  let newElement = document.createElement(type);
+  classArr.forEach((cssClass) =>
+    newElement.setAttribute('class', cssClass)
+  )
+  children.forEach((index) => {
+    if (typeof index === 'string') {
+      let newText = document.createTextNode(index);
+      newElement.appendChild(newText);
+    }
+    else {
+      newElement.appendChild(index)
+    }
+  });
+  return newElement;
 }
 
 const createDamArmString = (arr, obj) => {
-	let wDam;
-	if(arr === 'weapon') {
-		wDam = 'Damage: ' + obj.minDam + ' - ' + obj.maxDam + ' ' + obj.elemMod.elemType + " | Slot: " + obj.item.slot;
-	}
-	else {
-		wDam = 'Armor: ' + obj.armorValue + " | Slot: " + obj.item.slot;
-	}
-	return wDam;
+  let wDam;
+  if(arr === 'weapon') {
+    wDam = 'Damage: ' + obj.minDam + ' - ' + obj.maxDam + ' ' + obj.elemMod.elemType + " | Slot: " + obj.item.slot;
+  }
+  else {
+    wDam = 'Armor: ' + obj.armorValue + " | Slot: " + obj.item.slot;
+  }
+  return wDam;
 }
 
 const determineRarity = (obj, arr) => {
-	const namePanel = arr[0];
-	if(obj.elemMod.name != "" || obj.prefix.name != "" || obj.suffix.name != "") {
-		namePanel.style.backgroundColor = '#206720';
-		if(obj.elemMod.name != "" && obj.prefix.name != "" || obj.elemMod.name != "" && obj.suffix.name !="" ||
-		obj.prefix.name != "" && obj.suffix.name != "") {
-			namePanel.style.backgroundColor = '#2424a2';
-		}
-		if(obj.elemMod.name != "" && obj.prefix.name != "" && obj.suffix.name != "") {
-			namePanel.style.backgroundColor = 'Purple';
-			if(obj.qualityLevel >= 1000) {
-				namePanel.style.backgroundColor = '#d35d13';
-			}
-		}
-	}
+  const namePanel = arr[0];
+  if(obj.elemMod.name != "" || obj.prefix.name != "" || obj.suffix.name != "") {
+    namePanel.style.backgroundColor = '#206720';
+    if(obj.elemMod.name != "" && obj.prefix.name != "" ||
+    obj.elemMod.name != "" && obj.suffix.name !="" ||
+    obj.prefix.name != "" && obj.suffix.name != "") {
+      namePanel.style.backgroundColor = '#2424a2';
+    }
+    if(obj.elemMod.name != "" && obj.prefix.name != "" && obj.suffix.name != "") {
+      namePanel.style.backgroundColor = 'Purple';
+      if(obj.qualityLevel >= 1000) {
+        namePanel.style.backgroundColor = '#d35d13';
+      }
+    }
+  }
 }
 
 gId('itemGen').addEventListener('click', () => {
-	$.ajax({
-		url: "/weapon",
-		dataType: "json"
-	})
-	.done(function(data) {
-		gId('weaponContainer').innerHTML = '';
-		const weaponDiv = document.createElement('div');
-		for(let i = 0; i < 6; i++) {
-			const itemTypeValue = randomNum(0, (Object.keys(data.itemTypes).length - 1));
-			console.log("=========================");
-			console.log("Data for " + Object.keys(data.itemTypes)[itemTypeValue] + " #" + (i + 1));
-			console.log("=========================");
-			const itemProps = [];
-			const newItem = generateWeapon(data, Object.keys(data.itemTypes)[itemTypeValue]);
-			const wName = createComponent('p', [newItem.name], ['wName']);
-			itemProps.push(wName);
-			determineRarity(newItem, itemProps);
+  $.ajax({
+    url: "/weapon",
+    dataType: "json"
+  })
+  .done(function(data) {
+    gId('weaponContainer').innerHTML = '';
+    const weaponDiv = document.createElement('div');
+    for(let i = 0; i < 6; i++) {
+      const itemTypeValue = randomNum(0, (Object.keys(data.itemTypes).length - 1));
+      console.log("=========================");
+      console.log("Data for " + Object.keys(data.itemTypes)[itemTypeValue] + " #" + (i + 1));
+      console.log("=========================");
+      const itemProps = [];
+      const newItem = generateWeapon(data, Object.keys(data.itemTypes)[itemTypeValue]);
+      const wName = createComponent('p', [newItem.name], ['wName']);
+      itemProps.push(wName);
+      determineRarity(newItem, itemProps);
 
-			const weaponDam = createComponent('p', [createDamArmString(newItem.item.type, newItem)]);
-			itemProps.push(weaponDam);
+      const weaponDam = createComponent('p', [createDamArmString(newItem.item.type, newItem)]);
+      itemProps.push(weaponDam);
 
-			if(Object.keys(newItem.modifiers).length > 0) {
-				const mods = prettifyModifiers(Object.keys(newItem.modifiers), newItem.modifiers);
-				const wStat = createComponent('div', mods.map((mod) => createComponent('p', [mod], ['modString'])), ['stat-container']);
-				itemProps.push(wStat);
-			}
+      if(Object.keys(newItem.modifiers).length > 0) {
+        const mods = prettifyModifiers(Object.keys(newItem.modifiers), newItem.modifiers);
+        const wStat = createComponent('div', mods.map((mod) => createComponent('p', [mod], ['modString'])), ['stat-container']);
+        itemProps.push(wStat);
+      }
 
-			const wCost = createComponent('p', ['Cost: ' + newItem.cost + 'g']);
-			itemProps.push(wCost);
+      const wCost = createComponent('p', ['Cost: ' + newItem.cost + 'g']);
+      itemProps.push(wCost);
 
-			const innerContainer = createComponent('div', itemProps, ['weaponContainer']);
-			weaponDiv.appendChild(innerContainer);
-		}
-		gId('weaponContainer').appendChild(weaponDiv);
-	})
+      const innerContainer = createComponent('div', itemProps, ['weaponContainer']);
+      weaponDiv.appendChild(innerContainer);
+    }
+    gId('weaponContainer').appendChild(weaponDiv);
+  })
 });
